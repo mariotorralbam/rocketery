56d22d124603ea02d81d25b4bbb206d4d6357a65
Mario <mariotorrabam@protonmail.com>
2021-03-15 14:16:04 +0100

    Limpieza de memoria

---
+++ src/CleanEEPROM/CleanEEPROM.ino
@@ -0,0 +1,27 @@
+/*
+ * EEPROM Clear
+ *
+ * Sets all of the bytes of the EEPROM to 0.
+ * Please see eeprom_iteration for a more in depth
+ * look at how to traverse the EEPROM.
+ *
+ * This example code is in the public domain.
+ */
+
+#include <EEPROM.h>
+
+void setup() {
+  // initialize the LED pin as an output.
+  pinMode(13, OUTPUT);
+
+  for (int i = 0 ; i < EEPROM.length() ; i++) {
+    EEPROM.write(i, 0);
+  }
+
+  // turn the LED on when we're done
+  digitalWrite(13, HIGH);
+}
+
+void loop() {
+  /** Empty loop. **/
+}
